babd900e39aa68ea4104b2aca33a00d8c2ee4de8
fsalinas26 <salinasf3@gmail.com>
2021-08-27 18:43:14 -0700

    Declared GET functions w & w/o headers

--- Login Form/Server.cpp
+++ Login Form/Server.cpp
@@ -1,11 +1,30 @@
 #include "Server.h"
+#include "lw_http.hpp"
 
+//*GET
+//Content type will default to application/x-www-form-urlencoded if no headers are provided
 void Server::GET(std::wstring URL, std::string& response)
 {
+	c_lw_http http_obj;
+	if (!(http_obj.open_session()))
+	{
+		return;//socket failed to open
+	}
+	http_obj.get(URL, response);
+	http_obj.close_session();
 }
 
+//*GET w/ headers
+//Use header param to declare Authentication fields (i.e. bearer token) and content-type
 void Server::GET(std::wstring URL, std::string& response, std::vector<std::wstring>& headers)
 {
+	c_lw_http http_obj;
+	if (!(http_obj.open_session()))
+	{
+		return;//socket failed to open
+	}
+	http_obj.get(URL, response,headers);
+	http_obj.close_session();
 }
 
 void Server::POST(std::wstring URL, std::vector<post_request> body, std::string& response)
--- Login Form/Server.h
+++ Login Form/Server.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "lw_http.hpp"
+
 
 struct post_request {
 	char* field1;
@@ -9,6 +9,6 @@ class Server {
 public: 
 	void GET(std::wstring URL, std::string& response);
 	void GET(std::wstring URL, std::string& response,std::vector<std::wstring> &headers);
-	void POST(std::wstring URL, std::vector<post_request> body, std::string& response);
-	void POST(std::wstring URL, std::vector<post_request> body, std::string& response, std::vector<std::wstring>& headers);
+	void POST(std::wstring URL, std::vector<post_request> body, std::string &response);
+	void POST(std::wstring URL, std::vector<post_request> body, std::string &response, std::vector<std::wstring> &headers);
 };
