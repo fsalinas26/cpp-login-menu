a632cfcccf02d712ef7515839b06e4b914b3888f
fsalinas26 <salinasf3@gmail.com>
2021-09-02 12:50:26 -0700

    Added Change Rank and Extend License functionality

---
+++ NodeJS Server/Commands/ChangeRank.js
@@ -0,0 +1,27 @@
+module.exports = {
+name: "rank",
+adminOnly: true,
+execute(db,body,res,adminMode){
+    new Promise(resolve=>{
+        db.serialize(function(){
+            db.get("SELECT * FROM Users WHERE Username = ?",[body.username], async function(err,row)
+            {
+                if(row)
+                {
+                    db.run("UPDATE Users SET Rank = ? WHERE Username = ?",[body.newRank,body.username],function(err){
+                        if(err)
+                            resolve(err.message);
+                        else
+                            resolve(`${body.username} rank has been changed from ${row.Rank} to ${body.newRank}`);
+                    })
+                }
+                else{
+                    resolve("No user found");
+                }
+            })
+        });
+    }).then(response=>{
+        res.send(response);
+    })
+}
+}
---
+++ NodeJS Server/Commands/ExtendLicense.js
@@ -0,0 +1,29 @@
+const macros = require("../Macros");
+module.exports = {
+name: "extend",
+adminOnly: true,
+execute(db,body,res,adminMode){
+    new Promise(resolve=>{
+        db.serialize(function(){
+            db.get("SELECT * FROM Users WHERE Username = ?",[body.username], async function(err,row)
+            {
+                if(row)
+                {
+                    var extendDate = await macros.extendTime(row.Expiry,parseInt(body.extendBy));
+                    db.run("UPDATE Users SET Expiry = ? WHERE Username = ?",[extendDate,body.username],function(err){
+                        if(err)
+                            resolve(err.message);
+                        else
+                            resolve(`${body.extendBy} days have been added to ${body.username}. New Expiry ${extendDate}`);
+                    })
+                }
+                else{
+                    resolve("No user found");
+                }
+            })
+        });
+    }).then(response=>{
+        res.send(response);
+    })
+}
+}
