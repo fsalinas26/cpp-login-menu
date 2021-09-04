7efde1abc1d81fc60ff82ad506f92287fa7b8777
fsalinas26 <salinasf3@gmail.com>
2021-09-04 15:12:47 -0700

    cleanup

--- Login Form/CallbackThread.h
+++ Login Form/CallbackThread.h
@@ -1,19 +1,26 @@
 #pragma once
-
+#include "hwid.h"
 void ServerFetch(int command)
 {
+	g_response = "";
 	fetchingData = true;
 	switch (command)
 	{
 	case LOGIN:
-		Server::LOGIN(globalUser.username, globalUser.password, "3UI1LI891P1", g_response);
+		Server::LOGIN(globalUser.username, globalUser.password, hwid::get_hardware_id("1"), g_response);
 		Sleep(500);
 		break;
 	case REGISTER:
-		Server::REGISTER(globalUser.username, globalUser.password, "3UI1LI891P1", globalUser.variable, g_response);
+		Server::REGISTER(globalUser.username, globalUser.password, hwid::get_hardware_id("1"), globalUser.variable, g_response);
 		Sleep(500);
 		break;
-	default:
+	case FORGOT:
+		Server::FORGOT(globalUser.username, globalUser.password, globalUser.variable, g_response);
+		Sleep(500);
+		break;
+	case RESET:
+		Server::RESET(globalUser.username, globalUser.password, g_response);
+		Sleep(500);
 		break;
 	}
 	fetchingData = false;
