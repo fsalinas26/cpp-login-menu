4603b5842c8126f17c0727b9e88291630c50c902
fsalinas26 <salinasf3@gmail.com>
2021-09-06 17:08:16 -0700

    Encryption and Decryption on reqs and responses

--- NodeJS Server/Server.js
+++ NodeJS Server/Server.js
@@ -3,14 +3,20 @@ const express = require('express'),
       router = express.Router();
 const config = require('./config.json');    
 const port = 80; 
-const fs = require("fs");
+
 const bodyParser = require('body-parser');
+
 const sqlite3 = require("sqlite3");
 const database_filepath = "./users.db";
 const db = new sqlite3.Database(database_filepath);
 
-let Commands = new Map();
+const crypto = require('./Encryption/crypto');
+const key = Buffer.from("oUHqJ9IOlyjA4edqmyFdkeNi8J/x+dte2AWlGRd2uTM=",'base64');
 
+
+
+let Commands = new Map();
+const fs = require("fs");
 function ImportCommands() {
     const commandFiles = fs.readdirSync('./Commands').filter(file => file.endsWith('.js'));
     for (const file of commandFiles) {
@@ -21,29 +27,37 @@ function ImportCommands() {
 }
 ImportCommands();
 
-//app.use(express.json());
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies 
 
 router.post("/post",async(req,res)=>{
-    console.log(req.body);
+    req.body = await crypto.decryptBody(req.body,key,req.body.iv); //decrypt the JSON Object request
     req.body.ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     const command = Commands.get(req.body.command);
+    let resObj = {};
     if(!command.adminOnly)
-        command.execute(db,req.body,res);
-    else
-        res.send("You do not have access to this Command");
+        resObj.res = await command.execute(db,req.body,resObj);
+    else 
+        resObj.res = "You do not have access to this command";
+    resObj = await crypto.encryptResponse(resObj,key,req.body.iv); //encrypt the JSON Object response
+    res.send(resObj);
 });
 
 router.post("/admin",async(req,res)=>{
     if(config.auth === req.headers.authorization)
     {
         const Database = Commands.get(req.body.command);
-        Database.execute(db,req.body,res);
+        var response = await Database.execute(db,req.body,res,true);
+        res.send(response);
     }else{
         res.send("Access Denied");
     }
 });
 
+app.get("/initialize",async(req,res)=>{
+    let iv = crypto.generateIV();
+    res.send(iv.toString('base64'));
+})
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies 
 app.use('/',router);
 app.listen(port, ()=>{console.log(`Listening on port ${port}`)});
