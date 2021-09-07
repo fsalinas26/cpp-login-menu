f427223a722043adf6d631b4c2cb5970f5fd3a1d
fsalinas26 <salinasf3@gmail.com>
2021-09-07 00:11:35 -0700

    Renamed LastHWIDReset to LastReset.

--- NodeJS Server/Commands/CreateDB.js
+++ NodeJS Server/Commands/CreateDB.js
@@ -3,7 +3,7 @@ const sqlite3 = require("sqlite3");
 module.exports = {
     name: "create",
     adminOnly: true,
-    execute(db,body,res){
+    execute(){
         return new Promise(resolve=>{
             let db = new sqlite3.Database("./users.db", (err)=>{
             if(err){
@@ -13,7 +13,7 @@ module.exports = {
         }); 
         
         db.serialize(function() {
-            db.run("CREATE TABLE Users (License UNIQUE, Username UNIQUE, Password TEXT, HWID TEXT, LastLogin TEXT, LastIP TEXT, Expiry TEXT, UserVar TEXT, Rank TEXT, LastHWIDReset TEXT)");
+            db.run("CREATE TABLE Users (License UNIQUE, Username UNIQUE, Password TEXT, HWID TEXT, LastLogin TEXT, LastIP TEXT, Expiry TEXT, UserVar TEXT, Rank INTEGER, LastHWIDReset TEXT)");
             
             db.each("SELECT * FROM Users", function(err, row) {
                 console.log(row);
